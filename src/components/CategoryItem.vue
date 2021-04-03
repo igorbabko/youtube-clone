51c33abf90978fa496c66bda36a70687c9e2d104
Igor Babko <i.i.babko@studnote.com>
2021-04-03 11:43:06 +0300

    4: Create category component

---
+++ src/components/CategoryItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <a href="#" :class="classes">
+    {{ category }}
+  </a>
+</template>
+
+<script>
+export default {
+  props: ['category', 'isActive'],
+
+  computed: {
+    classes () {
+      const classes = ['px-3', 'py-1', 'transition', 'rounded-full', 'border']
+
+      return this.isActive
+        ? [
+            ...classes,
+            'border-gray-700',
+            'bg-gray-600',
+            'hover:bg-gray-500',
+            'text-white'
+          ]
+        : [...classes, 'border-gray-300', 'bg-gray-100', 'hover:bg-gray-200']
+    }
+  }
+}
+</script>
--- src/components/TheCategories.vue
+++ src/components/TheCategories.vue
@@ -3,24 +3,26 @@
     class="pt-14 md:pl-24 xl:pl-64 w-full fixed bg-white bg-opacity-95 z-10"
   >
     <div class="border-t border-b px-4 max-w-screen-2xl m-auto">
-      <div
-        class="py-3 flex space-x-3 overflow-auto text-sm whitespace-nowrap"
-      >
-        <a
+      <div class="py-3 flex space-x-3 overflow-auto text-sm whitespace-nowrap">
+        <CategoryItem
           v-for="category in categories"
           :key="category"
-          href="#"
-          class="px-3 py-1 transition bg-gray-600 border border-gray-700 rounded-full hover:bg-gray-500 text-white"
-        >
-          {{ category }}
-        </a>
+          :category="category"
+          :is-active="category === 'All'"
+        />
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import CategoryItem from './CategoryItem.vue'
+
 export default {
+  components: {
+    CategoryItem
+  },
+
   data () {
     return {
       categories: [
