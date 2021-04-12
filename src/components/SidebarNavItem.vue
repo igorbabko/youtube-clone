b2e1bb95bf8cae438b7ad3fcd0cb5e240f2e48ba
Igor Babko <i.i.babko@studnote.com>
2021-04-12 20:50:09 +0300

    10: Create components for sidebar nav items

---
+++ src/components/SidebarNavItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <li>
+    <a href="#" :class="classes">
+      <BaseIcon :name="icon" :class="iconClasses" />
+      <span>{{ label }}</span>
+    </a>
+  </li>
+</template>
+
+<script>
+import BaseIcon from './BaseIcon.vue'
+
+export default {
+  components: {
+    BaseIcon
+  },
+
+  props: {
+    isActive: Boolean,
+    label: String,
+    icon: String
+  },
+
+  computed: {
+    classes () {
+      const classes = ['flex', 'items-center', 'px-6', 'py-2', 'text-sm']
+
+      return this.isActive
+        ? [
+            ...classes,
+            'font-medium',
+            'text-gray-800',
+            'bg-gray-200',
+            'hover:bg-gray-300'
+          ]
+        : [...classes, 'hover:bg-gray-100']
+    },
+
+    iconClasses () {
+      return this.isActive ? 'w-6 h-6 mr-6 text-red-500' : 'w-6 h-6 mr-6'
+    }
+  }
+}
+</script>
--- src/components/VideoItemInfo.vue
+++ src/components/VideoItemInfo.vue
@@ -11,7 +11,7 @@
         <span>Channel name {{ index }}</span>
         <BaseIcon name="checkCircle" class="w-3.5 h-3.5 ml-1" />
       </div>
-      <div>{{ index }}K views &middot; {{ index }} day ago</div>
+      <div v-html="summary"></div>
     </div>
     <button
       class="-mt-1 ml-auto p-1 opacity-0 group-hover:opacity-100 text-gray-500 hover:text-gray-700 focus:outline-none"
@@ -29,6 +29,14 @@ export default {
     BaseIcon
   },
 
-  props: ['index']
+  props: ['index'],
+
+  computed: {
+    summary () {
+      const days = this.index === 1 ? 'day' : 'days'
+
+      return `${this.index}K views &middot; ${this.index} ${days} ago`
+    }
+  }
 }
 </script>
