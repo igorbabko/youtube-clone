9725f237e0e7f174348d1b394cf064bbb7e8700f
Igor Babko <i.i.babko@studnote.com>
2021-04-20 20:05:07 +0300

    13: Closing animation of mobile sidebar

--- src/components/TheSidebarMobile.vue
+++ src/components/TheSidebarMobile.vue
@@ -1,15 +1,32 @@
 <template>
-  <TheSidebarMobileOverlay>
-    <aside class="w-64 max-h-screen overflow-auto bg-white">
+  <transition
+    enter-active-class="transition-opacity ease-linear duration-200"
+    enter-from-class="opacity-0"
+    enter-to-class="opacity-100"
+    leave-active-class="transition-opacity ease-linear duration-200"
+    leave-from-class="opacity-100"
+    leave-to-class="opacity-0"
+  >
+    <TheSidebarMobileOverlay @click="isOpen = false" v-show="isOpen" />
+  </transition>
+  <transition
+    enter-active-class="transition ease-in-out duration-200 transform"
+    enter-from-class="-translate-x-full"
+    enter-to-class="translate-x-0"
+    leave-active-class="transition ease-in-out duration-200 transform"
+    leave-from-class="translate-x-0"
+    leave-to-class="-translate-x-full"
+  >
+    <aside v-show="isOpen" class="w-64 max-h-screen overflow-auto bg-white fixed z-40">
       <section class="flex items-center p-4 border-b sticky top-0 bg-white">
-        <button class="ml-2 mr-6 focus:outline-none">
+        <button @click="isOpen = false" class="ml-2 mr-6 focus:outline-none">
           <BaseIcon name="menu" />
         </button>
         <LogoMain />
       </section>
       <SidebarContent />
     </aside>
-  </TheSidebarMobileOverlay>
+  </transition>
 </template>
 
 <script>
@@ -24,6 +41,12 @@ export default {
     SidebarContent,
     LogoMain,
     TheSidebarMobileOverlay
+  },
+
+  data() {
+    return {
+      isOpen: true
+    }
   }
 }
 </script>
--- src/components/TheSidebarMobileOverlay.vue
+++ src/components/TheSidebarMobileOverlay.vue
@@ -1,5 +1,3 @@
 <template>
-  <div class="fixed inset-0 bg-black bg-opacity-50 z-30">
-    <slot />
-  </div>
+  <div class="fixed inset-0 bg-black bg-opacity-50 z-30"></div>
 </template>
