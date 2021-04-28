312faf18c2df104fa21746de61ccd193c94bb0e1
Igor Babko <i.i.babko@studnote.com>
2021-04-28 15:24:18 +0300

    17: Split compact sidebar into components

---
+++ src/components/SidebarCompactNavItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <li>
+    <a href="#" :class="classes">
+      <BaseIcon :name="icon" class="w-6 h-6 mb-1.5" />
+      <span class="text-xs">{{ label }}</span>
+    </a>
+  </li>
+</template>
+
+<script>
+import BaseIcon from './BaseIcon.vue'
+
+export default {
+  components: {
+    BaseIcon
+  },
+
+  props: {
+    isActive: Boolean,
+    label: String,
+    icon: String
+  },
+
+  computed: {
+    classes () {
+      const classes = [
+        'flex',
+        'flex-col',
+        'items-center',
+        'px-2',
+        'py-5',
+        'hover:bg-gray-100'
+      ]
+
+      return this.isActive ? [...classes, 'text-red-500'] : classes
+    }
+  }
+}
+</script>
