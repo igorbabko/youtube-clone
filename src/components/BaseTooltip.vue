6367786f8c5fee6916ed59616802fd44aea0456b
Igor Babko <i.i.babko@studnote.com>
2021-05-12 17:45:56 +0300

    25: Create tooltip component

---
+++ src/components/BaseTooltip.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="relative">
+    <div class="h-full" @mouseenter="toggle" @mouseleave="toggle">
+      <slot />
+    </div>
+    <div v-show="isShown" :class="classes">{{ text }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String
+  },
+
+  data () {
+    return {
+      isShown: false
+    }
+  },
+
+  computed: {
+    classes () {
+      return [
+        'bg-gray-600',
+        'bg-opacity-80',
+        'rounded-sm',
+        'text-white',
+        'text-xs',
+        'whitespace-nowrap',
+        'p-2',
+        'absolute',
+        'top-14',
+        'left-1/2',
+        'transform',
+        '-translate-x-1/2'
+      ]
+    }
+  },
+
+  methods: {
+    toggle () {
+      this.isShown = !this.isShown
+    }
+  }
+}
+</script>
--- src/components/TheSearchButton.vue
+++ src/components/TheSearchButton.vue
@@ -1,17 +1,21 @@
 <template>
-  <button
-    class="focus:outline-none flex items-center px-6 py-1 text-gray-600 bg-gray-100 border border-l-0 border-gray-300 rounded-tr-sm rounded-br-sm hover:bg-gray-200"
-  >
-    <BaseIcon name="search" class="w-5 h-5" />
-  </button>
+  <BaseTooltip text="Search">
+    <button
+      class="h-full focus:outline-none flex items-center px-6 py-1 text-gray-600 bg-gray-100 border border-l-0 border-gray-300 rounded-tr-sm rounded-br-sm hover:bg-gray-200"
+    >
+      <BaseIcon name="search" class="w-5 h-5" />
+    </button>
+  </BaseTooltip>
 </template>
 
 <script>
 import BaseIcon from './BaseIcon.vue'
+import BaseTooltip from './BaseTooltip.vue'
 
 export default {
   components: {
-    BaseIcon
+    BaseIcon,
+    BaseTooltip
   }
 }
 </script>
