4fa5c10ba880f51a5c63cc542409c04c05f70129
Igor Babko <i.i.babko@studnote.com>
2021-08-28 22:26:54 +0300

    add dropdown header component

--- src/components/TheDropdownSettingsAppearance.vue
+++ src/components/TheDropdownSettingsAppearance.vue
@@ -1,10 +1,8 @@
 <template>
-  <section class="flex border-b border-gray-200 text-black">
-    <button @click.stop="$emit('select-menu', 'main')" class="px-3 focus:outline-none">
-      <BaseIcon name="arrowLeft" />
-    </button>
-    <span class="py-3">Appearance</span>
-  </section>
+  <TheDropdownSettingsHeader
+    title="Appearance"
+    @back="$emit('select-menu', 'main')"
+  />
   <section class="py-2">
     <div class="text-gray-500 text-xs p-3">
       Setting applies to this browser only
@@ -21,12 +19,12 @@
 </template>
 
 <script>
-import BaseIcon from './BaseIcon.vue'
+import DropdownSettingsHeader from './DropdownSettingsHeader.vue'
 import DropdownSettingsListItem from './DropdownSettingsListItem.vue'
 
 export default {
   components: {
-    BaseIcon,
+    DropdownSettingsHeader,
     DropdownSettingsListItem
   },
 
