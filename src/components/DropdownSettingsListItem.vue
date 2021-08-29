ee88267bdefc4ae02a93e2ee590563c3493839aa
Igor Babko <i.i.babko@studnote.com>
2021-08-29 13:30:21 +0300

    32: Select list item

--- src/components/DropdownSettingsListItem.vue
+++ src/components/DropdownSettingsListItem.vue
@@ -1,8 +1,12 @@
 <template>
   <li>
     <a href="#" class="flex items-center px-4 py-2 text-sm hover:bg-gray-100">
-      <BaseIcon v-if="icon" :name="icon" class="w-6 h-6 mr-3 text-gray-400" />
-      <span>{{ label }}</span>
+      <BaseIcon
+        v-if="isIconShown"
+        :name="iconName"
+        class="w-5 h-5 mr-3 text-gray-400"
+      />
+      <span :class="{ 'ml-8': !isIconShown }">{{ label }}</span>
       <BaseIcon
         v-if="withSubMenu"
         name="chevronRight"
@@ -21,9 +25,23 @@ export default {
   },
 
   props: {
+    active: Boolean,
     label: String,
-    icon: String,
-    withSubMenu: Boolean
+    withSubMenu: Boolean,
+    icon: {
+      type: String,
+      default: 'check'
+    }
+  },
+
+  computed: {
+    isIconShown () {
+      return this.active || this.icon !== 'check'
+    },
+
+    iconName () {
+      return this.active ? 'check' : this.icon
+    }
   }
 }
 </script>
--- src/components/TheDropdownSettingsAppearance.vue
+++ src/components/TheDropdownSettingsAppearance.vue
@@ -1,36 +1,40 @@
 <template>
-  <section class="py-2 border-b">
+  <section class="flex border-b border-gray-200 text-black">
+    <button class="px-3 focus:outline-none">
+      <BaseIcon name="arrowLeft" />
+    </button>
+    <span class="py-3">Appearance</span>
+  </section>
+  <section class="py-2">
+    <div class="text-gray-500 text-xs p-3">
+      Setting applies to this browser only
+    </div>
     <ul>
       <DropdownSettingsListItem
-        v-for="listItem in listItems.slice(0, 8)"
-        :key="listItem.label"
-        :label="listItem.label"
+        v-for="(theme, themeId) in themes"
+        :key="themeId"
+        :label="theme"
+        :active="themeId === selectedThemeId"
+        @click="selectedThemeId = themeId"
       />
     </ul>
   </section>
 </template>
 
 <script>
+import BaseIcon from './BaseIcon.vue'
 import DropdownSettingsListItem from './DropdownSettingsListItem.vue'
 
 export default {
   components: {
+    BaseIcon,
     DropdownSettingsListItem
   },
 
   data () {
     return {
-      listItems: [
-        {
-          label: 'Use device theme',
-        },
-        {
-          label: 'Dark theme'
-        },
-        {
-          label: 'Light theme'
-        }
-      ]
+      selectedThemeId: 0,
+      themes: ['Use device theme', 'Dark theme', 'Light theme']
     }
   }
 }
