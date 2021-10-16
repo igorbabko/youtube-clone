6e02c8cd7eb19ec0054fe3e05755c96b13404064
Igor Babko <i.i.babko@studnote.com>
2021-10-16 14:59:26 +0300

    BaseModalOverlay - 55

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,11 +1,8 @@
 <template>
-  <div
-    class="fixed inset-0 z-10 bg-black bg-opacity-80"
-    tabindex="-1"
-    @keydown.esc="close"
-    @click.self="close"
-  >
-    <div class="bg-white max-w-sm mx-auto my-8">
+  <div class="fixed inset-0 z-10" tabindex="-1" @keydown.esc="close">
+    <BaseModalOverlay @click="close" />
+
+    <div class="bg-white max-w-sm mx-auto my-8 relative">
       <div class="p-2 text-right">
         <BaseModalButtonClose @click="close" />
       </div>
@@ -16,11 +13,13 @@
 
 <script>
 import BaseModalButtonClose from './BaseModalButtonClose.vue'
+import BaseModalOverlay from './BaseModalOverlay.vue'
 import BaseModalBody from './BaseModalBody.vue'
 
 export default {
   components: {
     BaseModalButtonClose,
+    BaseModalOverlay,
     BaseModalBody
   },
 
