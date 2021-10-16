a5b86df4f2d621830ff889a3b9688628c155d3f9
Igor Babko <i.i.babko@studnote.com>
2021-10-16 14:03:34 +0300

    BaseModalButtonClose - 55

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="fixed inset-0 z-10 bg-black bg-opacity-80">
     <div class="bg-white max-w-sm mx-auto my-8">
+      <div class="p-2 text-right">
+        <BaseModalButtonClose />
+      </div>
       <div class="p-6">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
         voluptate modi quibusdam alias, beatae tenetur quia eos totam nam iure
@@ -12,3 +15,13 @@
     </div>
   </div>
 </template>
+
+<script>
+import BaseModalButtonClose from './BaseModalButtonClose.vue'
+
+export default {
+  components: {
+    BaseModalButtonClose
+  }
+}
+</script>
