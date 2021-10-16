fe80ec07d7b2513470edf3664846b4cebd1e9817
Igor Babko <i.i.babko@studnote.com>
2021-10-16 19:52:20 +0300

    voice modal content (slot) - 60

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -17,12 +17,7 @@
         <BaseModalButtonClose @click="close" />
       </div>
       <div class="p-6">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi sunt
-        sed dolores minus dicta praesentium ab ipsa, necessitatibus deserunt
-        aperiam exercitationem nostrum maiores odio tempore eaque rerum?
-        Distinctio maxime veniam optio quos quo facere molestias totam!
-        Veritatis saepe aliquid id suscipit! Officia laudantium nesciunt
-        expedita libero, facere modi. Rerum, laudantium!
+        <slot />
       </div>
     </div>
   </div>
---
+++ src/components/TheModalSearchWithVoice.vue
@@ -0,0 +1,20 @@
+<template>
+  <BaseModal>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi sunt sed
+    dolores minus dicta praesentium ab ipsa, necessitatibus deserunt aperiam
+    exercitationem nostrum maiores odio tempore eaque rerum? Distinctio maxime
+    veniam optio quos quo facere molestias totam! Veritatis saepe aliquid id
+    suscipit! Officia laudantium nesciunt expedita libero, facere modi. Rerum,
+    laudantium!
+  </BaseModal>
+</template>
+
+<script>
+import BaseModal from './BaseModal.vue'
+
+export default {
+  components: {
+    BaseModal
+  }
+}
+</script>
