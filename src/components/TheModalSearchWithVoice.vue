b533f963f310e358b5d9fc54dd9c8450323af1d7
Igor Babko <i.i.babko@studnote.com>
2021-10-31 20:48:00 +0200

    voice button - 61 - wip

--- src/components/TheModalSearchWithVoice.vue
+++ src/components/TheModalSearchWithVoice.vue
@@ -1,12 +1,16 @@
 <template>
   <BaseModal>
-    <p class="text-2xl mb-52">Microphone off. Try again.</p>
-    <button
-      class="w-16 h-16 mx-auto bg-gray-300 rounded-full text-black flex justify-center items-center relative focus:outline-none"
-    >
-      <BaseIcon name="microphone" />
-    </button>
-    <div class="text-center text-sm text-gray-500 mt-4">
+    <p class="text-2xl mb-52">{{ text }}</p>
+    <div class="flex justify-center items-center">
+      <span
+        v-show="isListening"
+        class="animate-ping absolute w-14 h-14 rounded-full border border-gray-300"
+      ></span>
+      <button :class="buttonClasses" @click="isListening = !isListening">
+        <BaseIcon name="microphone" />
+      </button>
+    </div>
+    <div :class="buttonHintClasses">
       Tap the microphone to try again
     </div>
   </BaseModal>
@@ -20,6 +24,43 @@ export default {
   components: {
     BaseModal,
     BaseIcon
+  },
+
+  data () {
+    return {
+      isListening: false
+    }
+  },
+
+  computed: {
+    text () {
+      return this.isListening ? 'Listening...' : 'Microphone off. Try again.'
+    },
+
+    buttonClasses () {
+      return [
+        this.isListening ? 'bg-red-700' : 'bg-gray-300',
+        this.isListening ? 'text-white' : 'text-black',
+        'w-16',
+        'h-16',
+        'rounded-full',
+        'flex',
+        'justify-center',
+        'items-center',
+        'relative',
+        'focus:outline-none'
+      ]
+    },
+
+    buttonHintClasses () {
+      return [
+        this.isListening ? 'invisible' : 'visible',
+        'text-center',
+        'text-sm',
+        'text-gray-500',
+        'mt-4'
+      ]
+    }
   }
 }
 </script>
