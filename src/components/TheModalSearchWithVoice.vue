e8d5905e58dcc7b50c1c01cb0e0c79fbbfe36928
Igor Babko <i.i.babko@studnote.com>
2021-11-11 12:22:07 +0200

    61: Animate microphone button

--- src/components/TheModalSearchWithVoice.vue
+++ src/components/TheModalSearchWithVoice.vue
@@ -1,12 +1,13 @@
 <template>
   <BaseModal>
-    <p class="text-2xl mb-52">Microphone off. Try again.</p>
-    <button
-      class="w-16 h-16 mx-auto bg-gray-300 rounded-full text-black flex justify-center items-center relative focus:outline-none"
-    >
-      <BaseIcon name="microphone" />
-    </button>
-    <div class="text-center text-sm text-gray-500 mt-4">
+    <p class="text-2xl mb-52">{{ text }}</p>
+    <div class="flex justify-center items-center">
+      <span v-show="isListening" :class="buttonAnimationClasses" />
+      <button :class="buttonClasses" @click="isListening = !isListening">
+        <BaseIcon name="microphone" />
+      </button>
+    </div>
+    <div :class="buttonHintClasses">
       Tap the microphone to try again
     </div>
   </BaseModal>
@@ -20,6 +21,56 @@ export default {
   components: {
     BaseModal,
     BaseIcon
+  },
+
+  data () {
+    return {
+      isListening: false
+    }
+  },
+
+  computed: {
+    text () {
+      return this.isListening ? 'Listening...' : 'Microphone off. Try again.'
+    },
+
+    buttonClasses () {
+      return [
+        this.isListening ? 'bg-red-600' : 'bg-gray-300',
+        this.isListening ? 'text-white' : 'text-black',
+        'w-16',
+        'h-16',
+        'mx-auto',
+        'rounded-full',
+        'flex',
+        'justify-center',
+        'items-center',
+        'relative',
+        'focus:outline-none'
+      ]
+    },
+
+    buttonHintClasses () {
+      return [
+        this.isListening ? 'invisible' : 'visible',
+        'text-center',
+        'text-sm',
+        'text-gray-500',
+        'mt-4'
+      ]
+    },
+
+    buttonAnimationClasses () {
+      return [
+        'animate-ping',
+        'absolute',
+        'w-14',
+        'h-14',
+        'rounded-full',
+        'border',
+        'border-gray-300'
+      ]
+    }
   }
 }
 </script>
