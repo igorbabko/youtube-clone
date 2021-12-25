ba2ea249ca29d4e1e1d3c89aa0799b54df0acfbd
Igor Babko <i.i.babko@studnote.com>
2021-12-25 23:59:35 +0200

    66 - footer slot

--- src/components/TheModalReportSearchPredictions.vue
+++ src/components/TheModalReportSearchPredictions.vue
@@ -1,6 +1,19 @@
 <template>
   <BaseModal>
     <p class="text-2xl mb-52">Report search predictions</p>
+    <template v-slot:footer="{ close }">
+      <button
+        @click="close"
+        class="uppercase text-sm font-medium tracking-wider py-2.5 px-4 ml-auto mr-1 focus:outline-none"
+      >
+        Cancel
+      </button>
+      <button
+        class="uppercase text-sm font-medium tracking-wider py-2.5 px-4 ml-1 mr-1 text-gray-400 cursor-auto focus:outline-none"
+      >
+        Report
+      </button>
+    </template>
   </BaseModal>
 </template>
 
