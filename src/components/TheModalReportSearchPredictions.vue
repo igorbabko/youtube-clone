69986cd8be008a7cfa7abfdd18fbd9fdd3fed49d
Igor Babko <i.i.babko@studnote.com>
2021-12-26 20:36:34 +0200

    67 - search predictions checkboxes

--- src/components/TheModalReportSearchPredictions.vue
+++ src/components/TheModalReportSearchPredictions.vue
@@ -1,6 +1,24 @@
 <template>
-  <BaseModal>
-    <p class="text-2xl mb-52">Report search predictions</p>
+  <BaseModal class="md:w-4/5 max-w-2xl">
+    <p class="text-base text-black tracking-wide">Report search predictions</p>
+    <div class="space-y-3 text-black text-sm mt-5">
+      <div
+        v-for="(text, index) in searchPredictions"
+        :key="text"
+        class="flex items-center cursor-pointer"
+      >
+        <input
+          type="checkbox"
+          class="h-5 w-5 cursor-pointer"
+          :id="index"
+          :value="text"
+          v-model="selectedSearchPredictions"
+        />
+        <label :for="index" class="ml-4 cursor-pointer flex-grow">
+          {{ text }}
+        </label>
+      </div>
+    </div>
     <template v-slot:footer="{ close }">
       <button
         @click="close"
@@ -23,6 +41,28 @@ import BaseModal from './BaseModal.vue'
 export default {
   components: {
     BaseModal
+  },
+
+  data () {
+    return {
+      selectedSearchPredictions: [],
+      searchPredictions: [
+        'new york',
+        'new york song',
+        'new york alicia keys',
+        'new york live camera',
+        'new york state of mind',
+        'new york accent',
+        'new york nagaram whatsapp status',
+        'new york snng tik tok',
+        'new york new york frank sinatra',
+        'new york nagaram',
+        'new york jazz',
+        'new york full movie',
+        'new york vlog',
+        'new york jay z'
+      ]
+    }
   }
 }
 </script>
