e4c42d867b55f6a86596e16927144f595c77897a
Igor Babko <i.i.babko@studnote.com>
2021-12-27 16:30:59 +0200

    65: Create search predictions modal

--- src/components/TheSearchResults.vue
+++ src/components/TheSearchResults.vue
@@ -14,16 +14,37 @@
         </span>
       </li>
     </ul>
-    <a href="#" :class="reportLinkClasses">Report search predictions</a>
+    <a href="#" :class="reportLinkClasses" @click="openSearchPredictionsModal">
+      Report search predictions
+    </a>
+    <teleport to="body">
+      <TheModalSearchPredictions
+        v-if="isSearchPredictionsModalOpen"
+        @close="isSearchPredictionsModalOpen = false"
+      />
+    </teleport>
   </div>
 </template>
 
 <script>
+import TheModalSearchPredictions from './TheModalSearchPredictions.vue'
+
 export default {
+  components: {
+    TheModalSearchPredictions
+  },
+
   props: ['results', 'activeResultId'],
+
+  emits: [
+    'search-result-mouseenter',
+    'search-result-mouseleave',
+    'search-result-click'
+  ],
 
   data () {
     return {
+      isSearchPredictionsModalOpen: false,
       classes: [
         'absolute',
         'top-full',
@@ -58,6 +79,10 @@ export default {
         'select-none',
         'truncate'
       ]
+    },
+
+    openSearchPredictionsModal () {
+      this.isSearchPredictionsModalOpen = true
     }
   }
 }
