74fe3a58a2cd3a8471f5970993f75461f439f6b4
Igor Babko <i.i.babko@studnote.com>
2022-02-07 00:49:19 +0200

    78 - tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       transitionProperty: {
@@ -8,8 +10,4 @@ module.exports = {
       }
     },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 }
