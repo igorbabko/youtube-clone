586c47b0600799d748eb4888693d087799647bc2
Igor Babko <i.i.babko@studnote.com>
2022-02-07 00:03:27 +0200

    77 - textarea autogrow

--- src/components/BaseTextarea.vue
+++ src/components/BaseTextarea.vue
@@ -3,6 +3,7 @@
     <textarea
       v-bind="$attrs"
       v-model="modelValue"
+      @input="adjustHeight"
       :maxlength="limit"
       placeholder="Provide additional details (optional)"
       class="border rounded-sm pt-2 pb-5 px-2 resize-none w-full focus:outline-none peer"
@@ -25,6 +26,14 @@ export default {
   computed: {
     counter () {
       return `${this.modelValue.length} / ${this.limit}`
+    }
+  },
+
+  methods: {
+    adjustHeight ({ target: textarea }) {
+      textarea.style.overflow = 'hidden'
+      textarea.style.height = 'auto'
+      textarea.style.height = `${textarea.scrollHeight}px`
     }
   }
 }
