2520bcf17edc302363993d79c2e3e1d9ffba22f2
Igor Babko <i.i.babko@studnote.com>
2022-02-08 19:20:40 +0200

    WIP

--- src/components/BaseCheckbox.test.js
+++ src/components/BaseCheckbox.test.js
@@ -2,19 +2,39 @@ import { render, screen } from '@testing-library/vue'
 import BaseCheckbox from './BaseCheckbox.vue'
 
 test('renders checked base checkbox with label', () => {
+  const value = 1
+  const id = 'checkbox'
   const label = 'Test Label'
-  render(BaseCheckbox, {
+  const options = {
     props: {
-      id: 'checkbox',
-      value: 1,
-      modelValue: [1, 2, 3]
+      id,
+      value,
+      modelValue: [value, 2, 3]
     },
     slots: {
       default: label
     }
-  })
+  }
 
-  const checkbox = screen.getByLabelText(label)
+  render(BaseCheckbox, options)
 
-  expect(checkbox.checked).toBe(true)
+  // const checkbox = screen.getByLabelText(label)
+
+  // expect(checkbox.id).toBe(id)
+  expect(screen.getByLabelText(label).checked).toBe(true)
+})
+
+test('renders unchecked base checkbox without label', () => {
+  // const value = 1
+  const options = {
+    props: {
+      // value,
+      modelValue: [2, 3]
+    }
+  }
+
+  render(BaseCheckbox, options)
+
+  // expect(screen.getByDisplayValue(value).checked).toBe(false)
+  expect(screen.getByRole('checkbox').checked).toBe(false)
 })
