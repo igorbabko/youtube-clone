2d5ea5560a786adaf1704d5a6b58ecd4b0c3c268
Igor Babko <i.i.babko@studnote.com>
2022-02-12 23:24:37 +0200

    baseicon test (classes)

--- src/components/BaseIcon.test.js
+++ src/components/BaseIcon.test.js
@@ -11,7 +11,34 @@ test('renders icon', () => {
     }
 
     const { container } = render(BaseIcon, options)
-
+    
     expect(container.querySelector('path')).toBeTruthy()
   })
+})
+
+test('renders default icon classes', () => {
+  const defaultClasses = 'w-6 h-6'
+  const options = {
+    props: {
+      name: 'menu'
+    }
+  }
+
+  const { container } = render(BaseIcon, options)
+  
+  expect(container.querySelector('svg').getAttribute('class')).toBe(defaultClasses)
+})
+
+test('renders icon classes', () => {
+  const classes = 'w-10 h-10'
+  const options = {
+    props: {
+      name: 'menu',
+      class: classes
+    }
+  }
+
+  const { container } = render(BaseIcon, options)
+  
+  expect(container.querySelector('svg').getAttribute('class')).toBe(classes)
 })
