5ee400675ead4f2b039794e802cab2396b552d66
Igor Babko <i.i.babko@studnote.com>
2022-02-16 13:00:15 +0200

    2 - base button

--- src/components/BaseButton.test.js
+++ src/components/BaseButton.test.js
@@ -1,8 +1,78 @@
-import { render } from '@testing-library/vue'
+import { render, screen, prettyDOM } from '@testing-library/vue'
 import BaseButton from './BaseButton.vue'
 
 test('renders base button', () => {
-  const { debug } = render(BaseButton)
+  // ---------------------------------------------
+  // pretty dom ----------------------------------
+  // ---------------------------------------------
 
-  debug()
-})
+  // render(BaseButton)
+  // console.log(prettyDOM());
+
+  // ---------------------------------------------
+  // screen debug --------------------------------
+  // ---------------------------------------------
+  
+  // render(BaseButton)
+  // render(BaseButton)
+
+  // screen.debug()
+
+  // ---------------------------------------------
+  // render slot ---------------------------------
+  // ---------------------------------------------
+
+  // render(BaseButton, {
+  //   slots: {
+  //     default: 'Test Button'
+  //   }
+  // })
+
+  // screen.debug()
+
+  // ---------------------------------------------
+  // getByText -----------------------------------
+  // ---------------------------------------------
+
+  // render(BaseButton, {
+  //   slots: {
+  //     default: 'Test Button'
+  //   }
+  // })
+
+  // screen.getByText('Test Button')
+
+  // ---------------------------------------------
+  // getByText regexp ----------------------------
+  // ---------------------------------------------
+
+  // render(BaseButton, {
+  //   slots: {
+  //     default: 'Test Button'
+  //   }
+  // })
+
+  // screen.getByText(/Test Button/i)
+
+  // ---------------------------------------------
+  // getByText regexp ----------------------------
+  // ---------------------------------------------
+  
+  // given
+  // arrange
+  const options = {
+    slots: {
+      default: 'Test Button'
+    }
+  }
+
+  // when
+  // act
+  render(BaseButton, options)
+
+  // then
+  // assert
+  screen.getByText(/Test Button/i)
+  // screen.queryByText(/Test Button/i)
+  // console.log(screen)
+});
