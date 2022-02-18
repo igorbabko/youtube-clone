3e2121d3356ccf551efcdab5a456590cbcab1b2e
Igor Babko <i.i.babko@studnote.com>
2022-02-19 01:36:39 +0200

    5 - baseicon refactor

--- src/components/BaseIcon.test.js
+++ src/components/BaseIcon.test.js
@@ -2,53 +2,44 @@ import { render, screen } from '@testing-library/vue'
 import BaseIcon from './BaseIcon.vue'
 import icons from '../icons'
 
-test('renders icon', () => {
+const existingIconName = Object.keys(icons)[0]
+const nonExistentIconName = 'nonExistentIcon'
+
+function renderIcon(name, classes = null) {
   const options = {
     props: {
-      name: Object.keys(icons)[0]
+      name,
+      class: classes
     }
   }
 
-  render(BaseIcon, options)
+  return render(BaseIcon, options)
+}
+
+test('renders icon', () => {
+  renderIcon(existingIconName)
 
   expect(screen.getByTestId('base-icon').innerHTML).toBeTruthy()
 })
 
 test('renders non-existent icon', () => {
-  const options = {
-    props: {
-      name: 'nonExistentIcon'
-    }
-  }
-
-  render(BaseIcon, options)
+  renderIcon(nonExistentIconName)
  
   expect(screen.getByTestId('base-icon').innerHTML).toBeFalsy()
 })
 
 test('renders icon with default classes', () => {
   const defaultClasses = 'w-6 h-6'
-  const options = {
-    props: {
-      name: Object.keys(icons)[0]
-    }
-  }
 
-  render(BaseIcon, options)
+  renderIcon(existingIconName)
   
   expect(screen.getByTestId('base-icon').getAttribute('class')).toBe(defaultClasses)
 })
 
 test('renders icon with custom classes', () => {
   const classes = 'w-10 h-10'
-  const options = {
-    props: {
-      name: Object.keys(icons)[0],
-      class: classes
-    }
-  }
 
-  render(BaseIcon, options)
+  renderIcon(existingIconName, classes)
  
   expect(screen.getByTestId('base-icon').getAttribute('class')).toBe(classes)
 })
