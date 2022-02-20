caf7dd9a2770777709507d90a16b065ba490cb28
Igor Babko <i.i.babko@studnote.com>
2022-02-20 12:18:50 +0200

    7 [unit testing]: Refactor BaseIcon test

--- src/components/BaseIcon.test.js
+++ src/components/BaseIcon.test.js
@@ -2,53 +2,46 @@ import { render, screen } from '@testing-library/vue'
 import BaseIcon from './BaseIcon.vue'
 import icons from '../icons'
 
-test('renders icon', () => {
+const existingIconName = Object.keys(icons)[0]
+const nonExistentIconName = 'nonExistentIcon'
+
+function renderIcon(name, classes = null) {
   const options = {
     props: {
-      name: Object.keys(icons)[0]
+      name,
+      class: classes
     }
   }
 
-  render(BaseIcon, options)
+  return render(BaseIcon, options)
+}
+
+test('renders icon', () => {
+  renderIcon(existingIconName)
 
   expect(screen.getByTestId('base-icon').innerHTML).toBeTruthy()
 })
 
 test('renders non-existent icon', () => {
-  const options = {
-    props: {
-      name: 'nonExistentIcon'
-    }
-  }
-
-  render(BaseIcon, options)
+  renderIcon(nonExistentIconName)
 
   expect(screen.getByTestId('base-icon').innerHTML).toBeFalsy()
 })
 
 test('renders icon with default classes', () => {
-  const defaultClasses = 'w-6 h-6'
-  const options = {
-    props: {
-      name: Object.keys(icons)[0]
-    }
-  }
+  renderIcon(existingIconName)
 
-  render(BaseIcon, options)
+  const classes = screen.getByTestId('base-icon').getAttribute('class')
 
-  expect(screen.getByTestId('base-icon').getAttribute('class')).toBe(defaultClasses)
+  expect(classes).toBe('w-6 h-6')
 })
 
 test('renders icon with custom classes', () => {
-  const classes = 'w-10 h-10'
-  const options = {
-    props: {
-      name: Object.keys(icons)[0],
-      class: classes
-    }
-  }
+  const expectedClasses = 'w-10 h-10'
 
-  render(BaseIcon, options)
+  renderIcon(existingIconName, expectedClasses)
 
-  expect(screen.getByTestId('base-icon').getAttribute('class')).toBe(classes)
+  const classes = screen.getByTestId('base-icon').getAttribute('class')
+
+  expect(classes).toBe(expectedClasses)
 })
