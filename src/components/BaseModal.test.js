99e570b4ba21a223081e61e5fcfc6651f8fb6ad9
Igor Babko <i.i.babko@gmail.com>
2022-06-19 00:18:27 +0300

    12 [unit testing]: Refactor BaseModal test

--- src/components/BaseModal.test.js
+++ src/components/BaseModal.test.js
@@ -7,52 +7,51 @@ import {
 import BaseModal from './BaseModal.vue'
 import icons from '../icons'
 
-test('renders modal with body and footer', () => {
-  const body = 'This is modal body'
-  const footer = 'This is modal footer'
+function renderModal(body = '', footer = '', withCloseButton = false) {
   const options = {
+    props: {
+      withCloseButton
+    },
     slots: {
       default: body,
       footer
     }
   }
 
-  render(BaseModal, options)
+  return render(BaseModal, options)
+}
+
+test('renders modal with body and footer', () => {
+  const body = 'This is modal body'
+  const footer = 'This is modal footer'
+
+  renderModal(body, footer)
 
   screen.getByText(body)
   screen.getByText(footer)
 })
 
 test('renders modal with close button', () => {
-  const options = {
-    props: {
-      withCloseButton: true
-    }
-  }
+  const withCloseButton = true
 
-  render(BaseModal, options)
+  renderModal('', '', withCloseButton)
 
   expect(screen.getByTestId('base-icon').innerHTML).toBe(icons['x'])
 })
 
 test('renders modal without close button', () => {
-  render(BaseModal)
+  const withCloseButton = false
+
+  renderModal('', '', withCloseButton)
 
   expect(screen.queryByTestId('base-icon')).toBeNull()
 })
 
 test('closes modal when clicking close button', async () => {
   const body = 'This is modal body'
-  const options = {
-    props: {
-      withCloseButton: true
-    },
-    slots: {
-      default: body
-    }
-  }
+  const withCloseButton = true
 
-  render(BaseModal, options)
+  renderModal(body, '', withCloseButton)
 
   screen.getByText(body)
 
@@ -66,13 +65,8 @@ test('closes modal when clicking close button', async () => {
 
 test('closes modal when clicking overlay', async () => {
   const body = 'This is modal body'
-  const options = {
-    slots: {
-      default: body
-    }
-  }
 
-  render(BaseModal, options)
+  renderModal(body)
 
   screen.getByText(body)
 
@@ -86,18 +80,13 @@ test('closes modal when clicking overlay', async () => {
 
 test('closes modal when clicking cancel button in the footer', async () => {
   const body = 'This is modal body'
-  const options = {
-    slots: {
-      default: body,
-      footer: `
-        <template #footer="{ close }">
-          <button @click="close">Cancel</button>
-        </template>
-      `
-    }
-  }
+  const footer = `
+    <template #footer="{ close }">
+      <button @click="close">Cancel</button>
+    </template>
+  `
 
-  render(BaseModal, options)
+  renderModal(body, footer)
 
   screen.getByText(body)
 
@@ -111,13 +100,8 @@ test('closes modal when clicking cancel button in the footer', async () => {
 
 test('closes modal when pressing esc key', async () => {
   const body = 'This is modal body'
-  const options = {
-    slots: {
-      default: body
-    }
-  }
 
-  render(BaseModal, options)
+  renderModal(body)
 
   screen.getByText(body)
 
