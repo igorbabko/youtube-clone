2fa2917cb8e2034e50d5efbfcc50298aa7f6cdd0
Igor Babko <i.i.babko@gmail.com>
2022-06-22 08:57:28 +0300

    18 [unit testing]: Refactor TheModalSearchPredictions test

--- src/components/TheModalSearchPredictions.test.js
+++ src/components/TheModalSearchPredictions.test.js
@@ -1,84 +1,54 @@
-import { render, screen, fireEvent, waitForElementToBeRemoved } from '@testing-library/vue'
+import {
+  render,
+  screen,
+  fireEvent,
+  waitForElementToBeRemoved
+} from '@testing-library/vue'
 import TheModalSearchPredictions from './TheModalSearchPredictions.vue'
 
-it('shows search predictions', () => {
-  const searchPredictions = [
-    'Search Prediction 1',
-    'Search Prediction 2',
-    'Search Prediction 3'
-  ]
+function renderModal(searchPredictions, searchPredictionCategories) {
   const options = {
     props: {
       searchPredictions
-    }
+    },
+    data: () => ({
+      searchPredictionCategories
+    })
   }
 
-  render(TheModalSearchPredictions, options)
+  return render(TheModalSearchPredictions, options)
+}
 
-  screen.getByText(searchPredictions[0])
-  screen.getByText(searchPredictions[1])
-  screen.getByText(searchPredictions[2])
+it('shows search predictions', () => {
+  const predictions = ['Prediction 1', 'Prediction 2', 'Prediction 3']
+
+  renderModal(predictions)
+
+  predictions.forEach(screen.getByText)
 })
 
 it('shows search prediction categories', () => {
-  const searchPredictionCategories = [
-    'Search Prediction Category 1',
-    'Search Prediction Category 2',
-    'Search Prediction Category 3'
-  ]
-  const options = {
-    data: () => ({
-      searchPredictionCategories
-    })
-  }
+  const categories = ['Category 1', 'Category 2', 'Category 3']
 
-  render(TheModalSearchPredictions, options)
+  renderModal(categories)
 
-  screen.getByText(searchPredictionCategories[0])
-  screen.getByText(searchPredictionCategories[1])
-  screen.getByText(searchPredictionCategories[2])
+  categories.forEach(screen.getByText)
 })
 
 it('does not show search predictions if closed', () => {
-  const searchPredictions = [
-    'Search Prediction 1',
-    'Search Prediction 2',
-    'Search Prediction 3'
-  ]
-  const options = {
-    props: {
-      searchPredictions
-    }
-  }
-  render(TheModalSearchPredictions, options)
+  const predictions = ['Prediction 1', 'Prediction 2', 'Prediction 3']
+  renderModal(predictions)
 
   fireEvent.click(screen.getByRole('button', { name: 'Cancel' }))
 
-  return waitForElementToBeRemoved([
-    screen.queryByText(searchPredictions[0]),
-    screen.queryByText(searchPredictions[1]),
-    screen.queryByText(searchPredictions[2])
-  ])
+  return waitForElementToBeRemoved(predictions.map(screen.queryByText))
 })
 
 it('does not show search prediction categories if closed', () => {
-  const searchPredictionCategories = [
-    'Search Prediction Category 1',
-    'Search Prediction Category 2',
-    'Search Prediction Category 3'
-  ]
-  const options = {
-    data: () => ({
-      searchPredictionCategories
-    })
-  }
-  render(TheModalSearchPredictions, options)
+  const categories = ['Category 1', 'Category 2', 'Category 3']
+  renderModal(categories)
 
   fireEvent.click(screen.getByRole('button', { name: 'Cancel' }))
 
-  return waitForElementToBeRemoved([
-    screen.queryByText(searchPredictionCategories[0]),
-    screen.queryByText(searchPredictionCategories[1]),
-    screen.queryByText(searchPredictionCategories[2])
-  ])
+  return waitForElementToBeRemoved(categories.map(screen.queryByText))
 })
