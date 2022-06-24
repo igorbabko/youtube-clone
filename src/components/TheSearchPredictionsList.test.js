26675539aad895cecd8f5f2c4f85f9c31ace7455
Igor Babko <i.i.babko@gmail.com>
2022-06-24 10:35:42 +0300

    20 [unit testing]: Create TheSearchPredictionsList test

---
+++ src/components/TheSearchPredictionsList.test.js
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/vue'
+import TheSearchPredictionsList from './TheSearchPredictionsList.vue'
+
+const predictions = ['Prediction 1', 'Prediction 2', 'Prediction 3']
+const checkedPredictions = predictions.slice(1)
+
+function renderPredictions(checkedPredictions) {
+  const options = {
+    props: {
+      searchPredictions: predictions,
+      modelValue: checkedPredictions
+    }
+  }
+
+  return render(TheSearchPredictionsList, options)
+}
+
+it('shows search predictions unchecked', () => {
+  renderPredictions()
+
+  predictions.forEach(prediction =>
+    expect(screen.getByLabelText(prediction).checked).toBe(false)
+  )
+})
+
+it('shows specified search predictions checked', () => {
+  renderPredictions(checkedPredictions)
+
+  expect(screen.getByLabelText(predictions[0]).checked).toBe(false)
+  expect(screen.getByLabelText(checkedPredictions[0]).checked).toBe(true)
+  expect(screen.getByLabelText(checkedPredictions[1]).checked).toBe(true)
+})
