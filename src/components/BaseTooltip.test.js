0a35bd5776291d2a5199e01d05f6819b026f5e62
Igor Babko <i.i.babko@gmail.com>
2022-07-09 10:05:22 +0300

    30 [unit testing]: Test that tooltip hides

--- src/components/BaseTooltip.test.js
+++ src/components/BaseTooltip.test.js
@@ -41,3 +41,33 @@ it('shows after hovering over owning element', async () => {
 
   expect(screen.getByText(text)).toBeVisible()
 })
+
+it('hides after moving cursor away from owning element', async () => {
+  const text = 'Tooltip text'
+  const buttonLabel = 'Click Me'
+  const button = `<button>${buttonLabel}</button>`
+  renderTooltip(text, button)
+
+  await fireEvent.mouseEnter(screen.getByText(buttonLabel).parentElement)
+
+  expect(screen.getByText(text)).toBeVisible()
+
+  await fireEvent.mouseLeave(screen.getByText(buttonLabel).parentElement)
+
+  expect(screen.getByText(text)).not.toBeVisible()
+})
+
+it('hides after clicking owning element', async () => {
+  const text = 'Tooltip text'
+  const buttonLabel = 'Click Me'
+  const button = `<button>${buttonLabel}</button>`
+  renderTooltip(text, button)
+
+  await fireEvent.mouseEnter(screen.getByText(buttonLabel).parentElement)
+
+  expect(screen.getByText(text)).toBeVisible()
+
+  await fireEvent.click(screen.getByText(buttonLabel).parentElement)
+
+  expect(screen.getByText(text)).not.toBeVisible()
+})
