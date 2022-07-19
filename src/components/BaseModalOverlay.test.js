ec77b36f6bb792f16329485e23498cc98572b02c
Igor Babko <i.i.babko@gmail.com>
2022-07-19 12:51:35 +0300

    39 [unit testing]: Use toMatchInlineSnapshot assertion

--- src/components/BaseModalOverlay.test.js
+++ src/components/BaseModalOverlay.test.js
@@ -3,17 +3,6 @@ import BaseModalOverlay from './BaseModalOverlay.vue'
 
 it('renders correctly', () => {
   const { html } = render(BaseModalOverlay)
-  const comment = 'A very very long comment.'
-  const video = {
-    id: Math.ceil(Math.random() * 100),
-    title: 'Video Title',
-    publishedAt: new Date()
-  }
 
-  expect(html()).toMatchSnapshot()
-  expect(comment).toMatchSnapshot()
-  expect(video).toMatchSnapshot({
-    id: expect.any(Number),
-    publishedAt: expect.any(Date)
-  })
+  expect(html()).toMatchInlineSnapshot(`"<div class=\\"fixed inset-0 bg-black bg-opacity-80\\" data-testid=\\"base-modal-overlay\\"></div>"`)
 })
--- src/components/BaseTextareaCounter.test.js
+++ src/components/BaseTextareaCounter.test.js
@@ -10,5 +10,5 @@ it('renders correctly', () => {
 
   const { html } = render(BaseTextareaCounter, options)
 
-  expect(html()).toMatchSnapshot()
+  expect(html()).toMatchInlineSnapshot(`"<div class=\\"absolute bottom-2 right-2 text-xs text-gray-600 peer-focus:text-black\\">0 / 100</div>"`)
 })
