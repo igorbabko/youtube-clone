b32a9aeeb82e102c2f41999dc490da3386ffd125
Igor Babko <i.i.babko@gmail.com>
2022-07-23 00:53:58 +0300

    43 [unit testing]: Test DropdownSettingsListItem component

--- src/components/DropdownSettingsListItem.test.js
+++ src/components/DropdownSettingsListItem.test.js
@@ -2,12 +2,13 @@ import { render, screen } from '@testing-library/vue'
 import DropdownSettingsListItem from './DropdownSettingsListItem.vue'
 import icons from '../icons'
 
-function renderListItem(label = '', icon = null, withSubMenu = false) {
+function renderListItem(label = '', icon = null, withSubMenu = false, active = false) {
   const options = {
     props: {
       label,
       icon,
-      withSubMenu
+      withSubMenu,
+      active
     }
   }
 
@@ -36,4 +37,33 @@ it('renders with specified icon', () => {
   renderListItem('', icon)
 
   expect(screen.getByTestId('base-icon')).toContainHTML(icons[icon])
+})
+
+it('renders with specified icon and submenu icon', () => {
+  const icon = 'sun'
+  const withSubMenu = true
+
+  renderListItem('', icon, withSubMenu)
+
+  expect(screen.getAllByTestId('base-icon')).toHaveLength(2)
+  expect(screen.getAllByTestId('base-icon')[0]).toContainHTML(icons[icon])
+  expect(screen.getAllByTestId('base-icon')[1]).toContainHTML(icons['chevronRight'])
+})
+
+it('renders active with check icon', () => {
+  const active = true
+
+  renderListItem('', null, false, active)
+
+  expect(screen.getByTestId('base-icon')).toContainHTML(icons['check'])
+})
+
+it('renders active with check icon even if another icon was specified', () => {
+  const icon = 'sun'
+  const active = true
+
+  renderListItem('', icon, false, active)
+
+  expect(screen.getByTestId('base-icon')).toContainHTML(icons['check'])
+  expect(screen.getByTestId('base-icon')).not.toContainHTML(icons[icon])
 })
